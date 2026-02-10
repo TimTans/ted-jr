4bbe03c899539a710edd03df8eb5f3c6a55e2cf4
Jawad Chowdhury <124181330+JawadChy@users.noreply.github.com>
2026-02-10 13:47:12 -0500

    chore: init iOS project

---
+++ ios/Neighborly/Neighborly/NeighborlyApp.swift
@@ -0,0 +1,17 @@
+//
+//  NeighborlyApp.swift
+//  Neighborly
+//
+//  Created by Jawad Chowdhury on 2/10/26.
+//
+
+import SwiftUI
+
+@main
+struct NeighborlyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
